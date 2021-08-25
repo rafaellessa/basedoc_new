62dc879dd2cbfc54b7c5f1b2574a0f13b51df0d3
Rafael Lessa <rafael.lessa150@gmail.com>
2021-08-25 20:38:32 -0300

    feat: adjuste splash screen

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 import {Container, LogoBaseDoc} from './styles';
 
+import {useEffect} from 'react';
 import Logo from '../../assets/logo.png';
 
-const Splash: React.FC = () => {
+const Splash: React.FC = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('Signin', {});
+    }, 3000);
+  }, []);
+
   return (
     <Container>
       <LogoBaseDoc source={Logo} />
